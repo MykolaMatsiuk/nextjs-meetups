eb99ace5b4c2f76f7abe754d2f4c3ab492d1cde2
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2021-11-13 13:36:49 +0200

    getStaticPaths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,41 @@ function MeetupDetails() {
 	);
 }
 
+export async function getStaticPaths() {
+	return {
+		fallback: true,
+		paths: [
+			{
+				params: {
+					meetupId: 'm1'
+				}
+			},
+			{
+				params: {
+					meetupId: 'm2'
+				}
+			}
+		]
+	};
+}
+
+export async function getStaticProps(context) {
+	// fetch single meetup by id
+
+	const id = context.params.meetupId;
+
+	console.log(id);
+
+	return {
+		props: {
+			meetupdata: {
+				id,
+				image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Gauguin%2C_Paul_-_Landscape_near_Arles_-_Google_Art_Project.jpg/1280px-Gauguin%2C_Paul_-_Landscape_near_Arles_-_Google_Art_Project.jpg',
+				address: 'Some address',
+				description: 'Some meetup description'
+			}
+		}
+	};
+}
+
 export default MeetupDetails;
